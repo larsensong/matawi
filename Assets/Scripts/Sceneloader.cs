0e5ee3c0ea59fadfe18a9ac150af17b974d5336a
larsensong <larsenmulamba@gmail.com>
2023-02-05 09:57:31 +0300

    Polish

--- Assets/Scripts/Sceneloader.cs
+++ Assets/Scripts/Sceneloader.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Sceneloader : MonoBehaviour
 {
+    
     public void LoadNext()
     {
         int currentsceneindex = SceneManager.GetActiveScene().buildIndex;
@@ -14,5 +16,45 @@ public class Sceneloader : MonoBehaviour
     public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void LoadAboutScene()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void LoadHighscoresScene()
+    {
+        SceneManager.LoadScene("Highscores");
+    }
+
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+     public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadSecondLevel()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadThirdLevel()
+    {
+        SceneManager.LoadScene("Level 3");
     }
 }
